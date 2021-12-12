73bbb7192026db0220f9d5c642614c174be26519
plagocastro <plagocastro@danielcastelao.org>
2021-12-12 17:28:14 +0100

    Clase registro terminada

--- app/src/main/java/com/dam2/pmdm_agarimo/RegActivity.kt
+++ app/src/main/java/com/dam2/pmdm_agarimo/RegActivity.kt
@@ -1,18 +1,51 @@
 package com.dam2.pmdm_agarimo
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+
 
 class RegActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg)
 
+        val brRegistrar: Button = findViewById(R.id.bRegister2)
+        val brNombre: EditText = findViewById(R.id.name)
+        val brEmail: EditText = findViewById(R.id.emailAccess4)
+        val brContraseña: EditText = findViewById(R.id.passwordAccess)
 
+        val autentificacion: FirebaseAuth = FirebaseAuth.getInstance()
+
+        fun registrarUsuario() {
+            var name: String = brNombre.getText().toString()
+            var email: String = brEmail.getText().toString()
+            var password: String = brContraseña.getText().toString()
+
+            if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
+                if (password.length >= 6) {
+                    autentificacion.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful()) {
+                                Toast.makeText(this,"Registro completado correctamente",Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this,"No se pudo registrar el usuario",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this,"Por favor 6 caracteres mínimo",Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this,"Te faltan datos, completalos",Toast.LENGTH_SHORT).show()
+            }
+        }
+        brRegistrar.setOnClickListener() {
+            registrarUsuario()
+        }
+    }
 
 }
