8d6047f426b6cf7bed422dd582f25c4dcf6cd801
plagocastro <plagocastro@danielcastelao.org>
2021-12-12 16:53:31 +0100

    Terminado layout de registro y de logueo

---
+++ app/src/main/java/com/dam2/pmdm_agarimo/RegActivity.kt
@@ -0,0 +1,18 @@
+package com.dam2.pmdm_agarimo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+
+class RegActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg)
+
+
+
+}
