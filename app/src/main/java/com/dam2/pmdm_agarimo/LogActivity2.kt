839418ed2522e1e904936ecbc2b1635831a91f9d
plagocastro <plagocastro@danielcastelao.org>
2022-01-13 14:48:04 +0100

    adasdad

--- app/src/main/java/com/dam2/pmdm_agarimo/LogActivity2.kt
+++ app/src/main/java/com/dam2/pmdm_agarimo/LogActivity2.kt
@@ -4,32 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class LogActivity2 : AppCompatActivity() {
-    val mEmail: EditText = findViewById(R.id.emailAccess)
-    val mPassword: EditText = findViewById(R.id.passwordAccess)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log2)
 
-        val mLogin: Button = findViewById(R.id.bLogin)
+        val mEmail: EditText = findViewById(R.id.emailAccess)
+        val mPassword: EditText = findViewById(R.id.passwordAccess)
+        val rLogin: Button = findViewById(R.id.bLogin)
 
+        fun login() {
+            var email: String = mEmail.getText().toString()
+            var password: String = mPassword.getText().toString()
 
-        mLogin.setOnClickListener() {
-            login()
-        }
-
-    }
-
-    private fun login() {
-        var email: String = mEmail.getText().toString()
-        var password: String = mPassword.getText().toString()
-
-        if (!email.isEmpty() && !password.isEmpty()) {
-            if (password.length >= 6) {
-
+            if (!email.isEmpty() && !password.isEmpty()) {
+                if (password.length >= 6) {
+                } else {
+                    Toast.makeText(this, "Email o Contraseña incorrectos", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
+        }
+        rLogin.setOnClickListener() {
+            login()
         }
     }
 }
