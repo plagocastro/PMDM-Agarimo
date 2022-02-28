14947b3cb7a5621acc7437f9f2ead66894d3aa6c
plagocastro <plagocastro@danielcastelao.org>
2022-02-28 10:22:56 +0100

    Separados los registros de pacientes y de profesionales

--- app/src/main/java/com/dam2/pmdm_agarimo/MainActivity.kt
+++ app/src/main/java/com/dam2/pmdm_agarimo/MainActivity.kt
@@ -1,36 +1,30 @@
 package com.dam2.pmdm_agarimo
 
-
 import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import android.os.Bundle
+import android.util.Log.i
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView((R.layout.activity_main))
+        setContentView(R.layout.activity_main)
 
+        val mPaciente: Button = findViewById(R.id.bPaciente)
+        val mProfesional: Button = findViewById(R.id.bProfesional)
 
-        val mLogin: Button = findViewById(R.id.bLogin)
-        val mRegister: Button = findViewById(R.id.bRegister)
-
-        mLogin.setOnClickListener() {
-            Loguearse()
+        mPaciente.setOnClickListener() {
+            MenuSesion()
         }
-        mRegister.setOnClickListener() {
-            Registrarse()
+        mProfesional.setOnClickListener() {
+            MenuSesion()
         }
 
     }
 
-    private fun Registrarse() {
-        val intent = Intent(this, RegActivity::class.java)
+    private fun MenuSesion() {
+        val intent = Intent(this, MenuEleccion::class.java)
         startActivity(intent)
-    }
-    private fun Loguearse() {
-        val intent2 = Intent(this, LogActivity2::class.java)
-        startActivity(intent2)
     }
 }
---
+++ app/src/main/java/com/dam2/pmdm_agarimo/MenuEleccion.kt
@@ -0,0 +1,35 @@
+
+package com.dam2.pmdm_agarimo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuEleccion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView((R.layout.activity_menu_eleccion))
+
+
+        val mLogin: Button = findViewById(R.id.bLogin)
+        val mRegister: Button = findViewById(R.id.bRegister)
+
+        mLogin.setOnClickListener() {
+            Loguearse()
+        }
+        mRegister.setOnClickListener() {
+            Registrarse()
+        }
+
+    }
+
+    private fun Registrarse() {
+        val intent = Intent(this, RegActivity::class.java)
+        startActivity(intent)
+    }
+    private fun Loguearse() {
+        val intent2 = Intent(this, LogActivity2::class.java)
+        startActivity(intent2)
+    }
+}
